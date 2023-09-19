996a93e4ab015157f6843e80e0ba22cb473266b0
tony123x <tonychongng123@gmail.com>
2023-09-19 13:16:59 +0200

    proyectos json

--- src/components/Portafolio.js
+++ src/components/Portafolio.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import { trabajos } from './datos/trabajos';
+import {Link} from 'react-router-dom';
 
 export const Portafolio = () => {
   return (
-    <div>Portafolio</div>
+    <div>
+
+      {
+        trabajos.map( trabajo => {
+          return (
+            <article key={trabajo.id}>
+              <span>{trabajo.categorias}</span>
+              <h2><Link to={trabajo.url} target='__blank'>{trabajo.nombre}</Link></h2>
+              <h3>{trabajo.tecnologias}</h3>
+            </article>
+          )
+          
+        })
+      }
+
+
+
+    </div>
+
+    
   )
 }
---
+++ src/components/datos/trabajos.js
@@ -0,0 +1,25 @@
+export const trabajos = [
+    {
+        'id': 'movie-hub',
+        'nombre': 'Movie Hub',
+        'url': 'https://tonychongng.github.io/moviehub/',
+        'tecnologias': 'html, css, js, react',
+        'categorias': 'Desarrollo Web'
+    },
+
+    {
+        'id': 'todo-list',
+        'nombre': 'To-do List',
+        'url': 'https://tonychongng.github.io/todo_app/',
+        'tecnologias': 'html, css, js, react',
+        'categorias': 'Desarrollo Web'
+    },
+
+    {
+        'id': 'starbucks-clone',
+        'nombre': 'Starbucks Clone',
+        'url': 'https://tonychongng.github.io/Starbucks-Clone/',
+        'tecnologias': 'html, css, js',
+        'categorias': 'Desarrollo Web'
+    }
+];
