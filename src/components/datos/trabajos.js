29e1298af1be1bf038a5797f2b76eb8b963fa24f
tony123x <tonychongng123@gmail.com>
2023-09-26 12:09:48 +0200

    table fecth project

--- src/components/datos/trabajos.js
+++ src/components/datos/trabajos.js
@@ -1,8 +1,20 @@
 import moviehub from '../../images/moviehub.png';
 import todolist from '../../images/todolist.png';
 import starbucksclone from '../../images/starbucksclone.png';
+import table from '../../images/table.png';
 
 export const trabajos = [
+    {
+        'id': table,
+        'nombre': 'Table Fetch',
+        'url': 'https://tonychongng.github.io/moviehub/',
+        'code': 'https://github.com/tonychongng/table_fetch',
+        'demo': 'https://tonychongng.github.io/table_fetch/',
+        'tecnologias': 'HTML, CSS, JS, REACT',
+        'categorias': 'Web Development',
+        'descripcion': 'lorem ipsus olakese'
+
+    },
     {
         'id': moviehub,
         'nombre': 'Movie Hub',
@@ -10,7 +22,7 @@ export const trabajos = [
         'code': 'https://github.com/tonychongng/moviehub',
         'demo': 'https://tonychongng.github.io/moviehub/',
         'tecnologias': 'HTML, CSS, JS, REACT',
-        'categorias': 'Desarrollo Web',
+        'categorias': 'Web Development',
         'descripcion': 'lorem ipsus olakese'
     },
 
@@ -21,7 +33,7 @@ export const trabajos = [
         'tecnologias': 'HTML, CSS, JS, REACT',
         'code': 'https://github.com/tonychongng/todo_app',
         'demo': 'https://tonychongng.github.io/todo_app/',
-        'categorias': 'Desarrollo Web',
+        'categorias': 'Web Development',
         'descripcion': 'lorem ipsus olakese'
     },
 
@@ -32,7 +44,7 @@ export const trabajos = [
         'code': 'https://github.com/tonychongng/Starbucks-Clone',
         'demo': 'https://tonychongng.github.io/Starbucks-Clone/',
         'tecnologias': 'HTML, CSS, JS',
-        'categorias': 'Desarrollo Web',
+        'categorias': 'Web Development',
         'descripcion': 'lorem ipsus olakese'
     }
 ];
